360bb3281ff70fbc98aa70aef20f2e31f1c19b31
Michelangelo_Foresta <forestamichelangelo@gmail.com>
2022-04-21 19:21:48 -0400

    Properly indented.

--- contact.c
+++ contact.c
@@ -16,35 +16,36 @@ void contact_free(struct Contact *c) {
 // WRITE Fname,Lname and number TO POINTER OF STRUCT VARIABLE
 void contact_write_fname(struct Contact* c, const char* s) {
  	if(strlen(s)<1){
-	printf("The first name is required.\n");
-	return;
+		printf("The first name is required.\n");
+		return;	
 	}  
 	if(strlen(s)>=63){
-	printf("The first name should be less than 63 characters.\n");
-	return;
+		printf("The first name should be less than 63 characters.\n");
+		return;
 	}
 	strncpy(c->fname, s , strlen(s));
 }
 	
 void contact_write_lname(struct Contact* c, const char* s) {
  	if(strlen(s)<1){
-	printf("The last name is required.\n");
-	return; 
+		printf("The last name is required.\n");
+		return; 
 	}
 	if(strlen(s)>=63){
-	printf("The last name should be less than 63 characters.\n");
-	return;
+		printf("The last name should be less than 63 characters.\n");
+		return;
 	}
 	strncpy(c->lname, s ,strlen(s));
 }
 
 void contact_write_number(struct Contact* c, const char* s) {
 	if(strlen(s)<1){
-	printf("The phone number is required.\n");
-	return;} 
-	if(strlen(s)>12){
-	printf("The phone number should be less than 12 numbers.\n");
-	return;
+		printf("The phone number is required.\n");
+		return;
+	}
+ 	if(strlen(s)>12){
+		printf("The phone number should be less than 12 numbers.\n");
+		return;
 	}  
 	strncpy(c->number, s ,strlen(s));
 }
