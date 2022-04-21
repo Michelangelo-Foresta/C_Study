341f3c358b4fd589b326b15b5049444770a5fd17
Michelangelo Foresta <forestamichelangelo@gmail.com>
2022-04-21 17:39:11 -0400

    Fixed indentation.

--- contact.c
+++ contact.c
@@ -3,49 +3,49 @@
 #include <string.h>
 #include "contact.h"
 
+/*Pass POINTER TO STRUCT THROUGH MALLOC*/
 struct Contact* contact_new() {
        return malloc(sizeof(struct Contact));
 }
-/*Pass POINTER TO STRUCT THROUGH MALLOC*/
 
+/*FREE POINTER TO STRUCT FROM MALLOC*/
 void contact_free(struct Contact *c) {
 	free(c);
 }
-/*FREE POINTER TO STRUCT FROM MALLOC*/
 
-
-// WRITE INPUT TO POINTER OF STRUCT VARIABLE
+// WRITE Fname,Lname and number TO POINTER OF STRUCT VARIABLE
 void contact_write_fname(struct Contact* c, const char* s) {
- if(strlen(s)<1){
+ 	if(strlen(s)<1){
 	printf("The first name is required.\n");
 	return;
-}  
- if(strlen(s)>=63){
+	}  
+	if(strlen(s)>=63){
 	printf("The first name should be less than 63 characters.\n");
 	return;
+	}
+	strncpy(c->fname, s , strlen(s));
 }
-  strncpy(c->fname, s , strlen(s));
-}
+	
 void contact_write_lname(struct Contact* c, const char* s) {
- if(strlen(s)<1){
+ 	if(strlen(s)<1){
 	printf("The last name is required.\n");
 	return; 
-}
-if(strlen(s)>=63){
+	}
+	if(strlen(s)>=63){
 	printf("The last name should be less than 63 characters.\n");
 	return;
+	}
+	strncpy(c->lname, s ,strlen(s));
 }
-strncpy(c->lname, s ,strlen(s));
-}
+
 void contact_write_number(struct Contact* c, const char* s) {
- if(strlen(s)<1){
+	if(strlen(s)<1){
 	printf("The phone number is required.\n");
-	return;
-} 
-if(strlen(s)>12){
+	return;} 
+	if(strlen(s)>12){
 	printf("The phone number should be less than 12 numbers.\n");
 	return;
-}  
-strncpy(c->number, s ,strlen(s));
+	}  
+	strncpy(c->number, s ,strlen(s));
 }
 // END OF WRITE
