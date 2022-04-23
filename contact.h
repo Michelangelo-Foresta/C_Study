70f7b5a9a75e1b871e26fc85b196f0789d207449
Michelangelo_Foresta <forestamichelangelo@gmail.com>
2022-04-22 20:03:40 -0400

    defined variables Fname,Lname.Number.

--- contact.h
+++ contact.h
@@ -1,16 +1,19 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#define Fname_len 64
+#define Lname_len 64
+#define Number_len 12
 
 struct Contact {
-        char fname[64];
-        char lname[64];
-        char number[13];
+        char fname[Fname_len];
+        char lname[Lname_len];
+        char number[Number_len];
 };
 
 struct Contact* contact_new();
 void contact_free(struct Contact *c);
 
-void contact_write_fname(struct Contact* c, const char* s);
-void contact_write_lname(struct Contact* c, const char* s);
-void contact_write_number(struct Contact* c, const char* s);
+int contact_write_fname(struct Contact* c, const char* s);
+int contact_write_lname(struct Contact* c, const char* s);
+int contact_write_number(struct Contact* c, const char* s);
 #endif
