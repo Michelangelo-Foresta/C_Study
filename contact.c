f38fdf97bed7c253026275c5ce60f13f02446f8e
Michelangelo_Foresta <forestamichelangelo@gmail.com>
2022-04-24 13:58:50 -0400

    Added error returns for fname, lname and string lengths.

--- contact.c
+++ contact.c
@@ -18,39 +18,42 @@ void contact_free(struct Contact *c) {
 
 // WRITE Fname,Lname and number TO POINTER OF STRUCT VARIABLE
 int contact_write_fname(struct Contact* c, const char* s) {
- 	if(strlen(s)<1){
-		printf("The first name is required.\n");
+ 	if(strlen(s)<=1){
+		//printf("The first name is required.\n");
 		return Error_too_short;
-	}  
+	}
 	if(strlen(s)>=Fname_len){
-		printf("The first name should be less than 63 characters.\n");
+		//printf("The first name should be less than 63 characters.\n");
 		return Error_too_long;
 	}
 	strncpy(c->fname, s , strlen(s));
+	return 0;
 }
 	
 int contact_write_lname(struct Contact* c, const char* s) {
- 	if(strlen(s)<1){
-		printf("The last name is required.\n");
+ 	if(strlen(s)<=1){
+		//printf("The last name is required.\n");
 		return Error_too_short; 
 	}
 	if(strlen(s)>=Lname_len){
-		printf("The last name should be less than 63 characters.\n");
+		//printf("The last name should be less than 63 characters.\n");
 		return Error_too_long;
 	}
 	strncpy(c->lname, s ,strlen(s));
+	return 0;
 }
 
 int contact_write_number(struct Contact* c, const char* s) {
 	if(strlen(s)<1){
-		printf("The phone number is required.\n");
+		//printf("The phone number is required.\n");
 		return Error_too_short;
 	}
  	if(strlen(s)>Number_len){
-		printf("The phone number should be less than 12 numbers.\n");
+		//printf("The phone number should be less than 12 numbers.\n");
 		return Error_too_long;
 	}  
 	strncpy(c->number, s ,strlen(s));
+	return 0;
 }
 
 // END OF WRITE
