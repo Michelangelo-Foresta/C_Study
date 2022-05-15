2440cd01d42bb8b10df1dac3e1eac1205049888e
Michelangelo_Foresta <forestamichelangelo@gmail.com>
2022-05-15 16:57:23 -0400

    internal header file

---
+++ contact_internal.h
@@ -0,0 +1,14 @@
+#ifndef INTERNAL_HEADER
+#define Fname_len 64
+#define Lname_len 64
+#define Number_len 12
+#include "contact.h"
+
+struct Contact {
+        char fname[Fname_len];
+        char lname[Lname_len];
+        char number[Number_len];
+};
+
+struct Contact;
+#endif
