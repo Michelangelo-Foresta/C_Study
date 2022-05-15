8a07138fe990ac1fd29adee891d3d25b2d96db1e
Michelangelo_Foresta <forestamichelangelo@gmail.com>
2022-05-15 15:45:54 -0400

    updated private header file

--- contact.h
+++ contact.h
@@ -1,19 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-#define Fname_len 64
-#define Lname_len 64
-#define Number_len 12
 
-struct Contact {
-        char fname[Fname_len];
-        char lname[Lname_len];
-        char number[Number_len];
-};
 
-struct Contact* contact_new();
-void contact_free(struct Contact *c);
+struct Contact;
 
-int contact_write_fname(struct Contact* c, const char* s);
-int contact_write_lname(struct Contact* c, const char* s);
-int contact_write_number(struct Contact* c, const char* s);
+
 #endif
