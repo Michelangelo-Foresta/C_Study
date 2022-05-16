499ccb7f33fda26399a3ea214071e154d8a28034
Michelangelo_Foresta <forestamichelangelo@gmail.com>
2022-05-16 08:20:07 -0400

    readded functions i accidentaly erased.

--- contact.h
+++ contact.h
@@ -4,5 +4,10 @@
 
 struct Contact;
 
-
+struct Contact* contact_new();
+void contact_free(struct Contact *c);
+int contact_write_fname(struct Contact* c, const char* s);
+int contact_write_lname(struct Contact* c, const char* s);
+int contact_write_number(struct Contact* c, const char* s);
+int contact_write(struct Contact* c,void* fd);
 #endif
